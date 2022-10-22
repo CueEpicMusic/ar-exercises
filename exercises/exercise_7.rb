50382fcfa648db96fab51fdc4a064fe50eaf94ad
CueEpicMusic <chiatunglo@hotmail.com>
2022-10-22 19:50:10 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter store name: "
+@store_name = gets.chomp
+
+puts Store.create(name: @store_name).errors.full_messages
