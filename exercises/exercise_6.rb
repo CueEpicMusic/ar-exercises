b68166a4adaadac3706256b8f642b96e3ef0ce20
CueEpicMusic <chiatunglo@hotmail.com>
2022-10-22 18:44:17 +0000

    complete exercise 4 - 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Beryl", last_name: "London", hourly_rate: 45)
+@store1.employees.create(first_name: "Krystine", last_name: "Isbel", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Ashlee", last_name: "Jannett", hourly_rate: 70)
+@store2.employees.create(first_name: "Wildfrid", last_name: "Terry", hourly_rate: 40)
+@store2.employees.create(first_name: "Monday", last_name: "Louie", hourly_rate: 50)
